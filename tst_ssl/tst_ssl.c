2c9513763cf1178f1d1c5991e488b9b3d9baf48d
orit.mamalya <orit.mamalya@docusign.com>
2017-09-12 10:19:59 +0300

    Add hello All remark

--- tst_ssl/tst_ssl.c
+++ tst_ssl/tst_ssl.c
@@ -20,6 +20,9 @@ double my_time_diff(long *end, long *start)
 	return elapsed;
 }
 
+// Hello Alll!!!!!!!!!!!
+
+
 int open_ssl_connection(int threadId, int NumOfIterations, char *ip, char *user1, char* user2, int isSC, char *pass, char *userMedia)
 {
 	int i=0;
